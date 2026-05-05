08f0157e2e8dd368a9a6579be4a6f4c24b6ae08b
Attila Nemet <attila.nemet@gmail.com>
2026-05-05 15:48:36 -0400

    3-gen max 2 lines

--- cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/3-gen_key.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 ssh-keygen -t rsa -b 4096 -f "$1" -N ""
-# -t rsa → RSA key type
-# -b 4096 → 4096-bit key size
-# -f "$1" → output filename (your argument, e.g. new_key)
-# -N "" → empty passphrase (non-interactive, required for scripts)
